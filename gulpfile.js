a92fc9ac341ed87e51727d3d4a3dcdc7d0ad8a1d
korochev <myver33@gmail.com>
2022-02-13 17:54:17 +0300

    upd

--- gulpfile.js
+++ gulpfile.js
@@ -130,7 +130,7 @@ const gh = () => {
     return src('dest/build/**/*') 
         .pipe(ghPages())
         .on('end', function(){
-            console.log(stdout._events.data)
+            console.log(process.stdout)
         })
 };
 
