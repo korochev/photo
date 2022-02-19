993da441ca073b2fe2fc2fe4b74bfba7fcdab3b1
korochev <myver33@gmail.com>
2022-02-19 12:24:56 +0300

    upd

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,19 @@
+//##Подписчик на события##
+
 let eventCheckers = [
     ['#hfs', 'keydown'],
     ['.projects__toggle', 'focus'],
     ['[type = "radio"]', 'keydown'],
-    ['.projects__switch', 'blur']
+    ['.projects__switch', 'blur'],
+    ['.valid-input', 'blur'],
+    ['.valid-input', 'input'],
+    ['.valid-form', 'change']
 ];
 eventCheckers.forEach(function(item, i) {
     document.querySelectorAll(item[0]).forEach(function(el){el.addEventListener(item[1],function(event){
+
+        //##Фокус элементов навигации##
+
         if(i == 0) {
             if(event.key=='Enter'){
                 if(!this.checked){this.checked=!0}else{this.checked=!1}
@@ -34,8 +42,16 @@ eventCheckers.forEach(function(item, i) {
                 document.querySelector('.service-tab__switch_cat_gen').focus();      
             } 
         }
+        else if (i == 4 || i == 5) {
+            changeDefaultFormMessage(event); 
+        }
+        else if (i == 6) {
+            changeFormHandler(event); 
+        }
     })})
 })
+
+//##Инициализация Яндекс.Карты##
 
 // Функция ymaps.ready() будет вызвана, когда
 // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
@@ -47,10 +63,43 @@ function init(){
                 // Порядок по умолчанию: «широта, долгота».
         // Чтобы не определять координаты центра карты вручную,
         // воспользуйтесь инструментом Определение координат.
-        center: [55.769535,37.639985],
+        center: [55.76659735488058,37.63137024464414],
         // Уровень масштабирования. Допустимые значения:
         // от 0 (весь мир) до 19.
         zoom: 15,
         controls: []
     });
+}
+
+//##Валидация форм##
+
+function changeFormHandler(e) {
+    let iterate = e.currentTarget;
+
+    if (e.target.checkValidity()) {
+        iterate.querySelector('.valid-submit').removeAttribute('disabled');
+    }
+}
+
+function changeDefaultFormMessage(e) {
+    let verificationClass = 'valid-input_invalid';
+    let verificationAttr = 'data-invalid';
+    let iterate = e.target;
+    
+    if (e.type == 'input'){
+        iterate.setAttribute(verificationAttr, true);
+        if(iterate.classList.contains(verificationClass)){
+            iterate.classList.remove(verificationClass);
+        }
+    }
+    if (e.type == 'blur'){
+        if (e.target.getAttribute(verificationAttr)) {
+            e.preventDefault();
+            iterate.classList.add(verificationClass);
+            iterate.removeAttribute(verificationAttr);
+            if (!iterate.checkValidity()) {
+                iterate.setAttribute('aria-invalid', true);
+            }
+        }
+    }
 }
