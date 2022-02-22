ff281f5fb1f8ae0d80eb86c67625c1aa8fba4cd8
korochev <myver33@gmail.com>
2022-02-22 19:06:20 +0300

    upd

--- src/js/script.js
+++ src/js/script.js
@@ -63,26 +63,6 @@ eventCheckers.forEach(function(item, i) {
     })})
 })
 
-//##Инициализация Яндекс.Карты##
-
-// Функция ymaps.ready() будет вызвана, когда
-// загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-ymaps.ready(init);
-function init(){
-    // Создание карты.
-    var myMap = new ymaps.Map("map", {
-        // Координаты центра карты.
-                // Порядок по умолчанию: «широта, долгота».
-        // Чтобы не определять координаты центра карты вручную,
-        // воспользуйтесь инструментом Определение координат.
-        center: [55.76659735488058,37.63137024464414],
-        // Уровень масштабирования. Допустимые значения:
-        // от 0 (весь мир) до 19.
-        zoom: 15,
-        controls: []
-    });
-}
-
 //##Валидация форм##
 
 function changeFormHandler(e) {
@@ -120,7 +100,7 @@ function changeDefaultFormMessage(e) {
     }
 }
 
-/* ##Lazy Load## */
+/* ##Lazy Load Изображений## */
 
 document.addEventListener("DOMContentLoaded", function(event) {
     var lazyImages =[].slice.call(
@@ -147,9 +127,71 @@ document.addEventListener("DOMContentLoaded", function(event) {
         lazyImageObserver.observe(lazyImage);
        });
     } else {
-      // Not supported, load all images immediately
+
      lazyImages.forEach(function(image){
          image.nextElementSibling.src = image.nextElementSibling.dataset.srcset;
        });
      }
    });
+
+   /* ##Lazy Load Яндекс.Карты## */
+
+   // Получаем нужный элемент
+var element = document.querySelector('#map-container');
+
+var Visible = function (target) {
+  // Все позиции элемента
+  var targetPosition = {
+      top: window.pageYOffset + target.getBoundingClientRect().top,
+      left: window.pageXOffset + target.getBoundingClientRect().left,
+      right: window.pageXOffset + target.getBoundingClientRect().right,
+      bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+    },
+    // Получаем позиции окна
+    windowPosition = {
+      top: window.pageYOffset,
+      left: window.pageXOffset,
+      right: window.pageXOffset + document.documentElement.clientWidth,
+      bottom: window.pageYOffset + document.documentElement.clientHeight
+    };
+
+  if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+    targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+    targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+    targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+    // Если элемент полностью видно, то запускаем следующий код
+    console.clear();
+    console.log('Вы видите элемент :)');
+    
+    //##Инициализация Яндекс.Карты##
+
+    // Функция ymaps.ready() будет вызвана, когда
+    // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
+    ymaps.ready(init);
+    function init(){
+        // Создание карты.
+        var myMap = new ymaps.Map("map", {
+            // Координаты центра карты.
+                    // Порядок по умолчанию: «широта, долгота».
+            // Чтобы не определять координаты центра карты вручную,
+            // воспользуйтесь инструментом Определение координат.
+            center: [55.76659735488058,37.63137024464414],
+            // Уровень масштабирования. Допустимые значения:
+            // от 0 (весь мир) до 19.
+            zoom: 15,
+            controls: []
+        });
+    }
+  } else {
+    // Если элемент не видно, то запускаем этот код
+    console.clear();
+  };
+};
+
+// Запускаем функцию при прокрутке страницы
+window.addEventListener('scroll', function() {
+  Visible (element);
+});
+
+// А также запустим функцию сразу. А то вдруг, элемент изначально видно
+Visible (element);
